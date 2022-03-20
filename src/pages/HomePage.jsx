fcd567df2847c7ca01a1da710d243db40801b84a
Aniket <aniketsaste96@gmail.com>
2022-03-20 18:49:24 +0530

    Chakra Ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Box,
+  Container,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+//Box works same as div
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily=" 'Montserrat', sans-serif">
+          Real Time Chat
+        </Text>
+      </Box>
+      <Box
+        color="black"
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded" mt="1em">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>Login!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Sign up!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
